322747de251e7445c78a25e3a3f4f18729d2b4ed
LizChang <yutongch@usc.edu>
2020-08-10 01:16:04 -0700

    update data and boxplots

--- scripts/gene.py
+++ scripts/gene.py
@@ -1,14 +1,14 @@
 mydict = {}
 
-df = open("../raw_data/gse66890gpl6244PHENO.csv","r", encoding="utf8", errors='ignore')
+df = open("../raw_data/gse63042gpl9115PHENO.csv","r", encoding="utf8", errors='ignore')
 for line in df:
   fields = line.strip().split(",")
   sample = fields[0]
-  survivor = fields[3]
+  survivor = fields[28]
   mydict[sample] = survivor
 
-with open("../summary_data/GSE66890/LM22prediction66890.tsv","r") as celltype:
-     for line in celltype:
+with open("../summary_data/symb.tsv","r") as gene:
+     for line in gene:
           fields = line.strip().split("\t")
           sample = fields[0]
           if sample in mydict:
--- scripts/wilcox_boxplot.R
+++ scripts/wilcox_boxplot.R
@@ -9,3 +9,17 @@ for (i in 3:20){
   print(result)}
 boxplot(#column names
   ~Survivor,df, xlab = "", ylab = "")
+
+install.packages("ggpubr")
+library(ggpubr)
+
+p <- ggboxplot(lm22, x = "Survivor", y = "T_cells_CD4",
+                               order = c("YES", "NO"),
+                               color = "#2E86C1", add = "jitter",
+                               bxp.errorbar = TRUE, xlab = FALSE,
+                               ylab = "T cells (CD4+)") + 
+       font("ylab", size = 16) +
+       font("xy.text", size = 16)+
+       scale_x_discrete(labels = c("YES" = "Survived", "NO" = "Died")) + 
+       stat_compare_means(label = "p.format", label.x = 1.5, size = 6)
+ggsave("lm22CD4Tcells.png", path = "../figures/", dpi = 300)
